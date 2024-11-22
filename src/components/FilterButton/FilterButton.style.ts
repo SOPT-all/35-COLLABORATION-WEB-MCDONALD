f2a0562b24fc1949c0b44f2ff7f7db882536f405
gwagjiug <kwjo0228@naver.com>
2024-11-22 21:08:14 +0900

    style : filterButton 컴포넌트 퍼블리싱

---
+++ src/components/FilterButton/FilterButton.style.ts
@@ -0,0 +1,15 @@
+import { Theme, css } from '@emotion/react';
+
+export const ButtonContainer =
+  (isHighlighted: boolean) => (theme: Theme) => css`
+    display: flex;
+    padding: 0.8rem 1.2rem;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+    border-radius: 0.8rem;
+    border: 1px solid ${theme.colors.red01};
+    color: ${isHighlighted ? theme.colors.white : theme.colors.red02};
+    ${theme.fonts.body09};
+    background-color: ${isHighlighted ? theme.colors.red02 : ''};
+  `;
---
+++ src/components/FilterButton/FilterButton.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as S from './FilterButton.style';
+
+type ButtonProps = {
+  menuTitle: string;
+  isHighlighted?: boolean;
+};
+
+const FilterButton = ({ menuTitle, isHighlighted = false }: ButtonProps) => (
+  <div css={S.ButtonContainer(isHighlighted)}>{menuTitle}</div>
+);
+
+export default FilterButton;
