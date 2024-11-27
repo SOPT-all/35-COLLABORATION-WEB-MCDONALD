9cb7cf607f826738f4c73a98396b2cd9d3a1691f
imddoy <imddoy@duksung.ac.kr>
2024-11-28 08:02:08 +0900

    chore: 텍스트 압축 플러그인 설치

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
+import viteCompression from 'vite-plugin-compression';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +12,11 @@ export default defineConfig({
     }),
     tsconfigPaths(),
     svgr(),
+    viteCompression({
+      algorithm: 'gzip', // 압축 알고리즘 지정
+      ext: '.gz', // 압축된 파일 확장자
+      threshold: 10240, // 최소 파일 크기 (10KB 이상인 파일만 압축)
+    }),
   ],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
