77d46f2537e4d61894dcaa215f15ad521894292d
gwagjiug <kwjo0228@naver.com>
2024-11-30 02:40:15 +0900

    fix: build 에러 해결

--- src/assets/svgs/burgerImg/single/ImgMenu13.tsx
+++ src/assets/svgs/burgerImg/single/ImgMenu13.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import type { SVGProps } from 'react';
 const SvgImgMenu13 = (props: SVGProps<SVGSVGElement>) => (
   <svg
--- src/assets/svgs/burgerImg/single/ImgMenu15.tsx
+++ src/assets/svgs/burgerImg/single/ImgMenu15.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import type { SVGProps } from 'react';
 const SvgImgMenu15 = (props: SVGProps<SVGSVGElement>) => (
   <svg
